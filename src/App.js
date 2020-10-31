6a0e40426a778100cd8ab209ffeb792ee8ef6f82
vistritpandey <vistritpandey@gmail.com>
2020-10-31 09:09:40 +0530

    m

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <div className="app__header">
         <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" />
       </div>
-      <h1>Header</h1>
+      
+      <Post />
       <Post />
     </div>
   );
