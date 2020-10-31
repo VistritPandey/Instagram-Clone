5730f8ce0c4b68269a8a6e701f92e1095b20197c
vistritpandey <vistritpandey@gmail.com>
2020-10-31 08:58:30 +0530

    m

--- src/Post.js
+++ src/Post.js
@@ -6,7 +6,7 @@ function Post() {
         <div className="post">
             <h3>Username</h3>
             <img className="post__image" src="https://m.hindustantimes.com/rf/image_size_1200x900/HT/p2/2020/06/20/Pictures/_3fe51840-b31f-11ea-8315-f58cec6734ed.jpg" alt="" />
-            <h4>Username: caption</h4>
+            <h4 className="post__text"><strong>Username: </strong> caption goes rigth here</h4>
         </div>
     )
 }
