32e34c58b5ad533723c70639ff3fd081e39502bc
vistritpandey <vistritpandey@gmail.com>
2020-10-31 15:53:04 +0530

    firebase set up

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBQ6IGF_AZmA582zx6qnfsTOivZg43Q-2g",
+    authDomain: "instagram-vistrit.firebaseapp.com",
+    databaseURL: "https://instagram-vistrit.firebaseio.com",
+    projectId: "instagram-vistrit",
+    storageBucket: "instagram-vistrit.appspot.com",
+    messagingSenderId: "420177454301",
+    appId: "1:420177454301:web:bb1ee2dc95924574dc6734",
+    measurementId: "G-6VHZREFRTJ"
+  };
