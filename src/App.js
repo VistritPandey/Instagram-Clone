61015a2be7445891a347ed36128230ac311bbab1
vistritpandey <vistritpandey@gmail.com>
2020-10-31 08:20:45 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="app">
+      <div className="app__header">
+        <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" />
+      </div>
+      <Header />
     </div>
   );
 }
