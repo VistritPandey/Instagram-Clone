ba9aa7953ebb6eb4077931826c9bbad6da3ac46b
vistritpandey <vistritpandey@gmail.com>
2020-10-31 08:55:55 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Post from "./Post"
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
         <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" />
       </div>
       <h1>Header</h1>
+      <Post />
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -4,7 +4,9 @@ import './Post.css'
 function Post() {
     return (
         <div className="post">
-            
+            <h3>Username</h3>
+            <img className="post__image" src="https://m.hindustantimes.com/rf/image_size_1200x900/HT/p2/2020/06/20/Pictures/_3fe51840-b31f-11ea-8315-f58cec6734ed.jpg" alt="" />
+            <h4>Username: caption</h4>
         </div>
     )
 }
