753bc38de4d54351901727725f1ec629b96768bb
vistritpandey <vistritpandey@gmail.com>
2020-10-31 16:59:42 +0530

    m

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyBQ6IGF_AZmA582zx6qnfsTOivZg43Q-2g",
     authDomain: "instagram-vistrit.firebaseapp.com",
     databaseURL: "https://instagram-vistrit.firebaseio.com",
@@ -8,4 +10,10 @@ const firebaseConfig = {
     messagingSenderId: "420177454301",
     appId: "1:420177454301:web:bb1ee2dc95924574dc6734",
     measurementId: "G-6VHZREFRTJ"
-  };
+  });
+
+const storage = firebase.storage();
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth, storage};
