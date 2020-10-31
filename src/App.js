2a585257bd94919117263516fa94b0437e99c870
vistritpandey <vistritpandey@gmail.com>
2020-10-31 17:28:33 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
+import React from "react"
 import { useState, useEffect }  from 'react';
 import './App.css';
 import Post from "./Post"
 import { db } from './firebase'
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button } from "@material-ui/core";
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const [modalStyle] = React.useState(getModalStyle);
+  const classes = useStyles();
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false)
 
   useEffect(() =>{
       db.collection('posts').onSnapshot(snapshot => {
@@ -15,11 +48,25 @@ function App() {
       })
   },[]);
 
+  const signUp () => {
+
+  }
+
   return (
     <div className="app">
+    
+    <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <h2>Sign Up/ Login</h2>
+        </div>
+      </Modal>
       <div className="app__header">
         <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" />
       </div>
+      <Button>Sign Up/In</Button>
 
       {
         posts.map(({id, post} )=> (
