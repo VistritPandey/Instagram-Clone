028629dbf39b552b7d2930b951aa7c9096b28e05
vistritpandey <vistritpandey@gmail.com>
2020-11-01 09:14:08 +0530

    m

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -2,6 +2,7 @@ import { Button } from '@material-ui/core'
 import React, { useState } from 'react'
 import { storage, db } from './firebase'
 import firebase from "firebase"
+import './ImageUpload.css'
 
 function ImageUpload({username}) {
     const[image, setImage] = useState(null);
