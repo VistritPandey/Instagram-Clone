bdee0f6a6622af15364f94f24f1d03532268f432
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2021-02-02 07:15:16 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,7 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCQxzaLbmm3yJgbKmqjcBqKv4BYnwF2Yv8",
-  authDomain: "vistrit-insta.firebaseapp.com",
-  databaseURL: "https://vistrit-insta.firebaseio.com",
-  projectId: "vistrit-insta",
-  storageBucket: "vistrit-insta.appspot.com",
-  messagingSenderId: "987746635486",
-  appId: "1:987746635486:web:e8c0970fc431f8bc603c4f",
-  measurementId: "G-TJLQ9QR65F"
-
+    // use your own config file
   });
 
 const storage = firebase.storage();
